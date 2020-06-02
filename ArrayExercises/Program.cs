baaffa6697935c8174e6b934be01762a92d6fd15
caleban2006 <logan.jen.2005@gmail.com>
2020-06-02 16:36:15 -0400

    did some work on the array exercises

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -5,8 +5,20 @@ namespace ArrayExercises
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello Arrays!");
+        {           
+
+            int[] myArray = { 1, 1, 2, 3, 5, 8 };
+            
+            Console.WriteLine(Loops.GetLoop(myArray));
+            
+            //for (int i = 0; i < myArray.Length; i++)
+            //{
+            //    //Console.WriteLine(myArray[i]);
+            //    if (myArray[i] % 2 != 0) 
+            //    {
+            //        Console.WriteLine(myArray[i]);
+            //    };
+            //}
         }
     }
 }
