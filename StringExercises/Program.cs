048ae86e86c6e723526ae3aa09f61360292e90df
caleban2006 <logan.jen.2005@gmail.com>
2020-06-03 10:31:26 -0400

    added string class

--- StringExercises/Program.cs
+++ StringExercises/Program.cs
@@ -6,12 +6,15 @@ namespace StringExercises
     {
         static void Main(string[] args)
         {
-            string myString = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
-            //string[] myArray = myString.Split(" ");
-            //Console.WriteLine(string.Join(",", myArray));
 
-            string[] myArray = myString.Split(".");
-            Console.WriteLine(string.Join(",", myArray));
+            Console.WriteLine(StringToArray.GetArray());
+
+            //string myString = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+            ////string[] myArray = myString.Split(" ");
+            ////Console.WriteLine(string.Join(",", myArray));
+
+            //string[] myArray = myString.Split(".");
+            //Console.WriteLine(string.Join(",", myArray));
         }
     }
 }
---
+++ StringExercises/StringToArray.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringExercises
+{
+    class StringToArray
+    {
+        public static string GetArray()
+        {
+            string myString = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+            //string[] myArray = myString.Split(" ");
+            //Console.WriteLine(string.Join(",", myArray));
+
+            string[] myArray = myString.Split(".");
+            Console.WriteLine(string.Join(",", myArray));
+        }
+
+    }
+}
